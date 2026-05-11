1014928f8738b8e53c37ed0809bc0fd05910bf30
Drevanok <sauleliseoaparicio@gmail.com>
2026-05-10 21:47:22 -0700

    feat: add service and save/load data localStorage

--- src/app/components/dragonball/character-list/character-list.component.ts
+++ src/app/components/dragonball/character-list/character-list.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Character } from '../../../interfaces/character.interface';
 
 @Component({
   selector: 'dragonball-character-list',
